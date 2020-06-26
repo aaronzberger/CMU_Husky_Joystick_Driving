9a92cb87ced3d30e749a7635ccf63a3fd84961a5
aaronzberger <aaronzberger@gmail.com>
2020-06-26 11:45:28 -0400

    Fixed driving: added constant message publishing

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,21 @@
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Twist.h>
 
-ros::Publisher pubVel;
-
 //Joystick Mappings (Logitech F710)
+const int kButtonX = 0;
 const int kButtonA = 1;
 const int kButtonB = 2;
 const int kButtonY = 3;
-const int kButtonX = 4;
+
+const int kAxisLeftHorizontal = 0;
+const int kAxisLeftVertical = 1;
+const int kAxisRightHorizontal = 2;
+const int kAxisRightVertical = 3;
+
+geometry_msgs::Twist currentMessage;
 
 /**
- * @brief Callback function for joystick input
+ * @brief Update the velocity message to publish based on joystick input
  * 
  * @param msg the joystick message received from the /joy ros topic
  */
@@ -20,19 +25,19 @@ void joyCallBack(const sensor_msgs::Joy::ConstPtr &msg) {
     //Determine linear velocity based on forward(Y) and backwards(A) buttons
     double linVel {0.0};
 
-    if(msg->buttons.at(kButtonY) == 1) {
+    if(msg->buttons.at(kButtonA) == 1) {
         linVel = 1.0;
-    } else if(msg->buttons.at(kButtonA) == 1) {
+    } else if(msg->buttons.at(kButtonY) == 1) {
         linVel = -1.0;
     }
 
     //Determine linear velocity based on right(B) and left(X) buttons
     double angVel {0.0};
 
-    if(msg->buttons.at(kButtonB) == 1) {
-        angVel = 0.5;
-    } else if(msg->buttons.at(kButtonX) == 1) {
-        angVel = -0.5;
+    if(msg->buttons.at(kButtonX) == 1) {
+        angVel = 0.75;
+    } else if(msg->buttons.at(kButtonB) == 1) {
+        angVel = -0.75;
     }
 
     //Create a velocity message to publish to the husky
@@ -46,7 +51,7 @@ void joyCallBack(const sensor_msgs::Joy::ConstPtr &msg) {
     twist.angular.y = 0;
     twist.angular.z = angVel;
 
-    pubVel.publish(twist);
+    currentMessage = twist;
 }
 
 int main(int argc, char **argv) {
@@ -55,7 +60,14 @@ int main(int argc, char **argv) {
     ros::NodeHandle n;
 
     ros::Subscriber subJoy = n.subscribe<sensor_msgs::Joy>("/joy", 100, joyCallBack);
-    pubVel = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+    ros::Publisher pubVel = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+
+    while(ros::ok()) {
+        pubVel.publish(currentMessage);
+        ros::spinOnce();
+    }
+
+    ros::spin();
 
     return 0;
 }
